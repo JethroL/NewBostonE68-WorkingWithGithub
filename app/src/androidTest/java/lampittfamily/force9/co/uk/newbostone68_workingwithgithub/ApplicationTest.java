62d21e98f1332efcaa1fd2739c6c37d4f49fc399
Jethro <Jethro.Lampitt@gmail.com>
2016-02-04 20:19:32 +0000

    Added Loads of text Labels2

---
+++ app/src/androidTest/java/lampittfamily/force9/co/uk/newbostone68_workingwithgithub/ApplicationTest.java
@@ -0,0 +1,13 @@
+package lampittfamily.force9.co.uk.newbostone68_workingwithgithub;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ app/src/test/java/lampittfamily/force9/co/uk/newbostone68_workingwithgithub/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package lampittfamily.force9.co.uk.newbostone68_workingwithgithub;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
